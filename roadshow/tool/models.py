1ae26603efb15c3b9d4f6e62a01019abca3be653
jack.long7 <jack.richard.long@gmail.com>
2021-03-19 17:26:32 -0500

    Added front end from previous version

--- roadshow/tool/models.py
+++ roadshow/tool/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=20)
+    rank = models.CharField(max_length=5)
+
+
+class Plan_of_insntruction(models.Model):
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    lessons = models.IntegerField(default=0)
--- roadshow/tool/views.py
+++ roadshow/tool/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello world.  You're at the tools index.")
+    # Some code to get db info
+    return render(request, 'tool/index.html')
