64487d3b265edf342412ca64fb306c55a5367287
jack.long7 <jack.richard.long@gmail.com>
2021-03-28 23:06:00 -0500

    Changed method so uploaded data now generates an HTML table

--- roadshow/tool/static/tool/js/newJQuery.js
+++ roadshow/tool/static/tool/js/newJQuery.js
@@ -26,6 +26,7 @@ $(document).ready(function() {
                 data = $.csv.toArrays(csvData);
                 if (data && data.length > 0) {
                   alert('Imported -' + data.length + '- rows successfully!');
+                  generateHtmlTable(data)
                 } else {
                     alert('No data to import!');
                 }
@@ -35,4 +36,42 @@ $(document).ready(function() {
             };
         }
     }
+    // Method that creates HTML Table
+    function generateHtmlTable(data) {
+    var html = '<table  class="table table-condensed table-hover table-striped">';
+ 
+      if(typeof(data[0]) === 'undefined') {
+        return null;
+      } else {
+		$.each(data, function( index, row ) {
+		  //bind header
+		  if(index == 0) {
+			html += '<thead>';
+			html += '<tr>';
+			$.each(row, function( index, colData ) {
+				html += '<th>';
+				html += colData;
+				html += '</th>';
+			});
+			html += '</tr>';
+			html += '</thead>';
+			html += '<tbody>';
+		  } else {
+			html += '<tr>';
+			$.each(row, function( index, colData ) {
+				html += '<td>';
+				html += colData;
+				html += '</td>';
+			});
+			html += '</tr>';
+		  }
+		});
+		html += '</tbody>';
+		html += '</table>';
+		// alert(html);
+		$('#csv-display').append(html);
+	  }
+	}	
+
+    
 });
